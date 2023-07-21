ac6d825e72119982888c503b78e11a9d0ddcf2ce
CR9 <106897958+gebali4802@users.noreply.github.com>
2023-07-21 09:29:57 +0900

    main

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+import asyncio
+import FinanceDataReader
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+  async def message(games):
+    await client.wait_until_ready()
+    while not client.is_closed():
+        for game in games:
+            await client.change_presence(status = discord.Status.idle, activity = discord.Game(game))
+            await asyncio.sleep(10)
+  await message(["stock-bot", "실제 있는 주식 정보를 받아오는 봇입니다!"])
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("!검색"):
+        stockCode = message.content.split(" ")[1]
+        if stockCode.isdigit():
+            embed = discord.Embed(title="[ 검색 ]", description="멘탈 복구 성공!", color=0xfead67)
+            embed.set_image(url=f"")
+            await message.channel.send(embed=embed)
+
+client.run("token")
